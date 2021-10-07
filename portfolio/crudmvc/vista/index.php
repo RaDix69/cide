6902c3be56185cd36affefe38d70dc303e0db668
RaDix69 <ronal4456@gmail.com>
2021-10-07 16:44:16 -0500

    css

--- portfolio/crudmvc/vista/index.php
+++ portfolio/crudmvc/vista/index.php
@@ -4,12 +4,58 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mvc.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;1,600&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="img/logo.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <title>Document</title>
 </head>
 <body>
-<div class="div">
-    <table class="table">
-            <caption>LISTADO DE APRENDICES</caption>
+    <!--------------------------------------scroll bar --------------------------------->
+    <div id="scroll" class="scroll">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+
+    <!-------------------------------------- fin scroll bar --------------------------------->
+
+    <!--------------------------------------cargador --------------------------------->
+    <div id="div-cargador" class="div-cargador">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+    <!-------------------------------------- fin del cargador --------------------------------->
+   
+<div id="div-contenido" class="hide">
+        <div class="contenido-nav">
+            <ul>
+                <li><a href="http://localhost/cide/portfolio#banner"  onclick="toggleMenu();">Home</a></li>
+                <li><a href="http://localhost/cide/portfolio/#about"  onclick="toggleMenu();">About</a></li>
+                <li><a href="http://localhost/cide/portfolio/#services"  onclick="toggleMenu();">Servicios</a></li>
+                <li><a href="http://localhost/cide/portfolio/#proyectos"  onclick="toggleMenu();">Proyectos</a></li>
+                <li><a href="http://localhost/cide/portfolio/#contacto"  onclick="toggleMenu();">Contacto</a></li>
+                <li><a href="http://localhost/cide/portfolio/crudmvc/vista/index.php"  onclick="toggleMenu();">mvc</a></li>
+            </ul>
+        </div>
+    <!-------------------------------------- fin nav --------------------------------->
+        <div class="contenido-main">
+    <!-------------------------------------- topbar --------------------------------->
+            <div class="contenido-main__topbar">
+                <a href="#" class="contenido-main-topbar__logo">Portfolio</a>
+                <div class="contenido-main-topbar-toggle">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
+                </div>
+            </div>
+    <!-------------------------------------- topbar --------------------------------->
+    <section class="tabla adjust">
+    <table >
+
             <thead>
                 <tr>
                    <th>id</th>
@@ -20,7 +66,6 @@
                     <th>tipo</th>
                     <th>Actualizar</th>
                     <th>eliminar</th>
-
 
                 </tr>
             </thead>
@@ -35,14 +80,17 @@
         echo "<td>". $key->getapellido2(). "</td>".'<br>';
         echo "<td>". $key->getcorreo(). "</td>".'<br>';
         echo "<td>". $key->gettipo(). "</td>".'<br>';
-     
          echo "<td><a href='indexmodificar.php?id=".$key->getid()."'>Actualizar</a></td>";
-               
          echo "<td><a href='#"."'>Actualizar</a></td>  </tr>";
     }
     ?>
-        
+    <a href="indexinsertar.php" class="registrar">Registrar</a>
+    
     </table>
+    </section>
 </div>    
 </body>
+<script src="../../js/cargador.js" language="javascript" type="text/javascript"></script>
+<script src="../../js/nav.js" language="javascript" type="text/javascript"></script>
+<script src="https://code.jquery.com/jqery-3.4.1.js"></script>
 </html>
--- portfolio/crudmvc/vista/indexinsertar.php
+++ portfolio/crudmvc/vista/indexinsertar.php
@@ -4,36 +4,82 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mvc.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;1,600&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="img/logo.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <title>Document</title>
     <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-    <div class="wrap">
-    <form action="../controlador/controlRegistro.php" method="GET">
+     <!--------------------------------------scroll bar --------------------------------->
+     <div id="scroll" class="scroll">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
 
-    <label for="">id</label>    
-    <input type="text" name="id" id="id" class="form-control" display="none">
+    <!-------------------------------------- fin scroll bar --------------------------------->
 
-    <label for="">NOMBRE</label>    
-    <input type="text" name="nombre" id="nombre" class="form-control">
+    <!--------------------------------------cargador --------------------------------->
+    <div id="div-cargador" class="div-cargador">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+    <!-------------------------------------- fin del cargador --------------------------------->
+   
+<div id="div-contenido" class="hide">
+        <div class="contenido-nav">
+             <ul>
+                <li><a href="http://localhost/cide/portfolio#banner"  onclick="toggleMenu();">Home</a></li>
+                <li><a href="http://localhost/cide/portfolio/#about"  onclick="toggleMenu();">About</a></li>
+                <li><a href="http://localhost/cide/portfolio/#services"  onclick="toggleMenu();">Servicios</a></li>
+                <li><a href="http://localhost/cide/portfolio/#proyectos"  onclick="toggleMenu();">Proyectos</a></li>
+                <li><a href="http://localhost/cide/portfolio/#contacto"  onclick="toggleMenu();">Contacto</a></li>
+                <li><a href="http://localhost/cide/portfolio/crudmvc/vista/index.php"  onclick="toggleMenu();">mvc</a></li>
+            </ul>
+        </div>
+    <!-------------------------------------- fin nav --------------------------------->
+  <div class="contenido-main">
+    <!-------------------------------------- topbar --------------------------------->
+            <div class="contenido-main__topbar">
+                <a href="#" class="contenido-main-topbar__logo">Portfolio</a>
+                <div class="contenido-main-topbar-toggle">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
+                </div>
+            </div>
+    <!-------------------------------------- topbar --------------------------------->
+   <section>
+   
+    <form action="../controlador/controlRegistro.php" method="GET" class="formulario">
+    <div class="linea">
+ 
+    <input type="text" name="id" id="id" class="form-control" placeholder="id">
+
+    <input type="text" name="nombre" id="nombre" class="form-control" placeholder="NOMBRE">
     
-    <label for="">apellido1</label>    
-    <input type="text" name="apellido1" id="apellido1" class="form-control">
+    <input type="text" name="apellido1" id="apellido1" class="form-control" placeholder="apellido1">
     
-    <label for="">apellido2</label>    
-    <input type="text" name="apellido2" id="apellido2" class="form-control">
+    <input type="text" name="apellido2" id="apellido2" class="form-control" placeholder="apellido2">
 
-    <label for="">correo</label>    
-    <input type="text" name="correo" id="correo" class="form-control">
+    <input type="text" name="correo" id="correo" class="form-control" placeholder="correo">
     
-    <label for="">contraseña</label>    
-    <input type="text" name="contrasena" id="contrasena" class="form-control"> 
+    <input type="text" name="contrasena" id="contrasena" class="form-control" placeholder="contraseña"> 
 
-    <label for="">tipo</label>    
-    <input type="text" name="tipo" id="tipo">
+    <input type="text" name="tipo" id="tipo" placeholder="tipo">
 
     <input type="submit" name="boton" value="Insertar Registro" class="btn btn-primary">
+    </div>
     </form>
-    </div>     
+    </div>  
+    </section>  
 </body>
+<script src="../../js/cargador.js" language="javascript" type="text/javascript"></script>
+<script src="../../js/nav.js" language="javascript" type="text/javascript"></script>
+<script src="https://code.jquery.com/jqery-3.4.1.js"></script>
 </html>
