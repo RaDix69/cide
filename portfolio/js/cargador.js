1cf616e600e2ec1b59a907d18b4d4caf1c1dd582
RaDix69 <ronal4456@gmail.com>
2021-10-08 10:15:02 -0500

    cargador

---
+++ portfolio/js/cargador.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    var contenedor =document.getElementById('div-cargador');
+    contenedor.style.visibility = 'hidden';
+    contenedor.style.opacity= '0';
+    document.getElementById('div-contenido').className = 'div-contenido';
+}
